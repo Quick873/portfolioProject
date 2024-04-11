033d68629bbed9293df3bb8c67cbbc27b547a5a0
Quick873 <116125610+Quick873@users.noreply.github.com>
2024-04-10 20:46:32 -0500

    Done...Sorta

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("myButton");
+
+button.addEventListener("click", function() {
+  // Code to execute when the button is clicked
+    button.innerHTML = "Happy Coding!";
+});
